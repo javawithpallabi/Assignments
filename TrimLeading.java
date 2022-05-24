d7295bce8052dfe11a0f0d7a0a779e54644f9107
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2022-05-24 12:49:03 +0530

    Add files via upload

---
+++ TrimLeading.java
@@ -0,0 +1,14 @@
+public class TrimLeading {
+
+public static void main(String[] args)
+    {
+        String str = " Java Program ";
+
+        
+        String new_str = str.trim();
+
+      
+System.out.println("Original String: " + str);
+System.out.println("New String: " + new_str);
+    }
+}
